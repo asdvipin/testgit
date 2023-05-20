4fabb2d33a647edd8a0a1c70146f17240d8021f2
ashika34 <ashikaspraveen@gmail.com>
2023-05-20 13:43:05 +0530

    test22222

--- test.dart
+++ test.dart
@@ -1,14 +1,14 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("object");
+  
   stdout.write("fgfgrgfb");
   stdout.write("object");
-  stdout.write("hgh");
+  stdout.write("gggggg");
   stdout.write("hytyjuyj");
   stdout.write("hytyjuyj");
   stdout.write("hytyjuyj");
   stdout.write("hytyjuyj");
   stdout.writeln();
-  stdout.write("hello world");
+  stdout.write("hehhhhhh");
 }
